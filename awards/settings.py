3dca9f68286ecc4ac3232d53e4f84feadbaceb54
Fahari <karonjekevin67@gmail.com>
2019-03-16 22:20:44 +0300

    install bootstrap in app settings

--- awards/settings.py
+++ awards/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tuzo.apps.TuzoConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
