b028e41e2f3f590ce5b58d33676aa88b5f08c5d0
Fahari <karonjekevin67@gmail.com>
2019-03-16 21:40:27 +0300

    correct the tuzo app settings definition

--- awards/settings.py
+++ awards/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news.apps.NewsConfig',
+    'tuzo.apps.TuzoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
