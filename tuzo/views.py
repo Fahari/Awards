08319b33d934cad4b316b11e616ae54de3e2b3de
Fahari <karonjekevin67@gmail.com>
2019-03-17 20:17:17 +0300

    update profile update forms.

--- tuzo/views.py
+++ tuzo/views.py
@@ -13,8 +13,19 @@ def profile(request):
     profile = Profile.objects.all()
     pics = Image.objects.all()
 
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            # message.success(request, f'Your account has been updated')
+            return render(request,'registration/profile.html')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
 
     context = {
         'u_form':u_form,
