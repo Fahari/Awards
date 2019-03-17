24a0b5f75b1dd4dd6ab33ba5baba3a0f2cf9a788
Fahari <karonjekevin67@gmail.com>
2019-03-17 10:34:17 +0300

    define where profile pictures should be stored in the settings

--- awards/settings.py
+++ awards/settings.py
@@ -141,6 +141,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
