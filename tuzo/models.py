f5453a02f13b47a69078ea6c67be0bad51c6e3b0
Fahari <karonjekevin67@gmail.com>
2019-03-18 12:14:46 +0300

    create model post

--- tuzo/models.py
+++ tuzo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Image(models.Model):
@@ -15,3 +16,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
