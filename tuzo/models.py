513fadd73cc6e5cc3bde43c67c3979d4944bef32
Fahari <karonjekevin67@gmail.com>
2019-03-18 11:57:10 +0300

    profile can update successfuly

--- tuzo/models.py
+++ tuzo/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length =30)
